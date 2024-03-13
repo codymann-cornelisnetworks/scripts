b9cdc4e5338df502d4b0fc6dc84447af8d4aa965
Cody Mann <cody.mann@cornelisnetworks.com>
2024-03-13 12:19:05 -0400

    Updates.

--- compile_imb.sh
+++ compile_imb.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
 source ./setup_env.sh
+# source setup_env_b2b.sh
 
 make -C $IMB_DIR clean
-LD_LIBRARY_PATH=$LIBFAB_PATH/lib:$MPI_PATH/lib:$LD_LIBRARY_PATH CC=$MPI_PATH/bin/mpicc CXX=$MPI_PATH/bin/mpicxx make -C $IMB_DIR
+LD_LIBRARY_PATH=$LIBFAB_PATH/lib:$MPI_PATH/lib:$MPI_PATH/lib64:$LD_LIBRARY_PATH CC=$MPI_PATH/bin/mpicc CXX=$MPI_PATH/bin/mpicxx make -C $IMB_DIR
 
 if [[ $? -ne 0 ]]
 then
--- mpicc_wrapper.sh
+++ mpicc_wrapper.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
-source ./setup_env.sh
+# source ./setup_env.sh
+source setup_env_b2b.sh
 
-LD_LIBRARY_PATH=$LIBFAB_PATH/lib:$MPI_PATH/lib:$MPI_PATH/lib64:$LD_LIBRARY_PATH $MPI_PATH/bin/mpicc $MPI_PROGRAMS/$1.c -o $MPI_PROGRAMS/$1 
+LD_LIBRARY_PATH=$LIBFAB_PATH/lib:$MPI_PATH/lib:$LD_LIBRARY_PATH $MPI_PATH/bin/mpicc $MPI_PROGRAMS/$1.c -o $MPI_PROGRAMS/$1 
